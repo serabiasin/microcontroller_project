95b16c126e504583350bc665b81e1cc27f022c7c
serabiasin <ahmadalfiazdzikri@gmail.com>
2019-10-05 22:52:18 +0700

    update lcd project

---
+++ stm32f103/lcd_experiment/Inc/lib_lcd.h
@@ -0,0 +1,48 @@
+#ifndef LIB_LCD_H
+#include "stm32f1xx_hal.h"
+#define LIB_LCD_H
+
+
+typedef struct {
+        GPIO_TypeDef *GPIOSEKTOR;
+        uint16_t PIN;
+} PIN_PERIPHERAL;
+
+typedef struct {
+        PIN_PERIPHERAL enPin;
+        PIN_PERIPHERAL rsPin;
+        PIN_PERIPHERAL rwPin;
+
+        PIN_PERIPHERAL BIT0;
+        PIN_PERIPHERAL BIT1;
+        PIN_PERIPHERAL BIT2;
+        PIN_PERIPHERAL BIT3;
+        PIN_PERIPHERAL BIT4;
+        PIN_PERIPHERAL BIT5;
+        PIN_PERIPHERAL BIT6;
+        PIN_PERIPHERAL BIT7;
+
+} LCD_DRIVER;
+
+/*ESSENTIAL ROUTINE SET*/
+void sendInstruction(uint16_t instruct, LCD_DRIVER *this);
+void splitBit(uint8_t byte, LCD_DRIVER *this); // 8 bit format
+GPIO_PinState getState(uint8_t data, uint8_t bit);
+
+void turnOn(PIN_PERIPHERAL *this);
+void turnOff(PIN_PERIPHERAL *this);
+
+/**
+ * ROUTINE FOR LCD PROCESS
+ */
+void beginLCD(LCD_DRIVER *this);
+
+/*MISC Features for LCD*/
+void sendcharTolcd(const char *string, LCD_DRIVER *this);
+void resetDisplay(LCD_DRIVER *this);
+
+/* IMPLEMENT SOON */
+void setPosition(uint8_t x, uint8_t y);
+
+
+#endif
---
+++ stm32f103/lcd_experiment/Src/lib_lcd.c
@@ -0,0 +1,127 @@
+#include "lib_lcd.h"
+
+#define CLEAR_DISPLAY 0x01
+#define LCD_16X2 0x38
+#define LCD_16X1 0x0
+/**
+ *  to do :
+ *  bikin method untuk parsing string
+ *
+ * */
+
+/**
+ *  Mengirim Instruksi ke LCD
+ * @param instruct [kode instruksi register]
+ * @param this     [LCD_DRIVER Struct]
+ *
+
+ */
+
+void sendInstruction(uint16_t instruct, LCD_DRIVER *this) {
+        HAL_Delay(50);
+        turnOff(&this->rsPin);
+        turnOff(&this->rwPin);
+	turnOn(&this->enPin);
+
+        splitBit(instruct, this);
+
+        turnOff(&this->enPin);
+        HAL_Delay(50);
+}
+
+/**
+ * Menampilkan output ke LCD
+ * @param string [alphanumeric bisa berupa string maupun karakter]
+ * @param this   [object LCD_DRIVER]
+ */
+void sendcharTolcd(const char *string, LCD_DRIVER *this) {
+        turnOn(&this->enPin);
+        turnOn(&this->rsPin);
+        turnOff(&this->rwPin);
+        while (*string != '\0') {
+                splitBit(*string, this);
+                string++;
+        }
+        turnOff(&this->enPin);
+        turnOff(&this->rsPin);
+}
+
+GPIO_PinState getState(uint8_t data, uint8_t bit) {
+
+        if (data & bit) {
+                return GPIO_PIN_SET;
+        } else {
+                return GPIO_PIN_RESET;
+        }
+}
+
+/**
+ * split Byte into bit per GPIO
+ * @param byte [the data that want to be splited]
+ * @param this [LCD_DRIVER Object]
+ */
+void splitBit(uint8_t byte, LCD_DRIVER *this) {
+        HAL_Delay(50);
+
+        HAL_GPIO_WritePin(this->BIT0.GPIOSEKTOR, this->BIT0.PIN,
+                          getState(byte, 0x01));
+
+        HAL_GPIO_WritePin(this->BIT1.GPIOSEKTOR, this->BIT1.PIN,
+                          getState(byte, 0x02));
+
+        HAL_GPIO_WritePin(this->BIT2.GPIOSEKTOR, this->BIT2.PIN,
+                          getState(byte, 0x04));
+
+        HAL_GPIO_WritePin(this->BIT3.GPIOSEKTOR, this->BIT3.PIN,
+                          getState(byte, 0x08));
+        HAL_GPIO_WritePin(this->BIT4.GPIOSEKTOR, this->BIT4.PIN,
+                          getState(byte, 0x10));
+
+        HAL_GPIO_WritePin(this->BIT5.GPIOSEKTOR, this->BIT5.PIN,
+                          getState(byte, 0x20));
+
+        HAL_GPIO_WritePin(this->BIT6.GPIOSEKTOR, this->BIT6.PIN,
+                          getState(byte, 0x40));
+
+        HAL_GPIO_WritePin(this->BIT7.GPIOSEKTOR, this->BIT7.PIN,
+                          getState(byte, 0x80));
+
+        HAL_Delay(50);
+}
+
+/**
+ *
+ * Set value satu pin I/O
+ * @param this [LCD_DRIVER object,di spesifikkan ke Pin I/O ]
+ *
+ */
+void turnOn(PIN_PERIPHERAL *this) {
+        HAL_GPIO_WritePin(this->GPIOSEKTOR, this->PIN, GPIO_PIN_SET);
+        HAL_Delay(50);
+}
+
+/**
+ *
+ * [Reset value satu pin I/O]
+ * @param this [LCD_DRIVER object,di spesifikkan ke Pin I/O]
+ *
+ */
+void turnOff(PIN_PERIPHERAL *this) {
+        HAL_GPIO_WritePin(this->GPIOSEKTOR, this->PIN, GPIO_PIN_RESET);
+        HAL_Delay(50);
+}
+
+void beginLCD(LCD_DRIVER *this) {
+        HAL_Delay(100);
+       	// aktifkan function set, 1 baris saja
+	sendInstruction(LCD_16X2,this);
+        // enable blink,cursor,dan display
+       	sendInstruction(0x0F, this);
+	HAL_Delay(100);
+}
+
+void resetDisplay(LCD_DRIVER *this){
+	HAL_Delay(50);
+	sendInstruction(CLEAR_DISPLAY,this);
+	HAL_Delay(50);
+}
